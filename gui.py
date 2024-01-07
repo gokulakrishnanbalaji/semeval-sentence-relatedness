a503f19975bbb99aa80bdcce8233d8cb4de9895b
gokulakrishnanbalaji <gokulbalaji2408@gmail.com>
2024-01-07 23:28:24 +0530

    improved gui

--- gui.py
+++ gui.py
@@ -1,18 +1,33 @@
 import gradio as gr
 from save_model import load
+import numpy as np
+from sentence_transformers import util
 
 #create a drop down model and get the model name
-model_name = gr.Dropdown(["tel","tam","mal","kan","hin","ben","eng"],label="Select Language")
+lang_mapping={
+    'Amharic':'amh',
+    'Arabic':'arq',
+    'Armenian':'ary',
+    'English':'eng',
+    'Spanish':'esp',
+    'Hausa':'hau',
+    'Kinyarwanda':'kin',
+    'Marathi':'mar',
+    'Telugu':'tel'
+}
+lang_list = [lang for lang in lang_mapping]
+model_name_full = gr.Dropdown(lang_list,label="Select Language")
+model_name = lang_mapping[model_name_full]
 
-#load the model
-model = load(model_name)
 
 #get 2 text inputs
 text1 = gr.Textbox(lines=5,label="Text 1")
 text2 = gr.Textbox(lines=5,label="Text 2")
 
 #predict the score
-def predict_score(text1,text2):
+def predict_score(model_name,text1,text2):
+    model = load(model_name)
+
     embedding1 = model.encode([text1])
     embedding2 = model.encode([text2])
     score = util.cos_sim(embedding1, embedding2)[0][0]  # Access specific element
@@ -22,5 +37,5 @@ def predict_score(text1,text2):
 output = gr.Textbox(label="Score")
 
 #launch the app
-gr.Interface(fn=predict_score, inputs=[text1,text2], outputs=output, title="Semantic Textual Similarity", description="Semantic Textual Similarity between two sentences").launch()
+gr.Interface(fn=predict_score, inputs=[model_name,text1,text2], outputs=output, title="Semantic Textual Similarity").launch()
 
